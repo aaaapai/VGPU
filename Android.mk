2735402043b4e2324de0d5f79ced46dedce18195
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-16 10:26:12 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -85,7 +85,7 @@ LOCAL_SRC_FILES := \
     src/GLES/vulkan/utils.cpp
     
 
-LOCAL_CFLAGS = -fno-exceptions -fno-rtti -std=c++11 -Wall -D_GLIBCXX_USE_CXX11_ABI=1
+LOCAL_CFLAGS = -fexceptions -frtti -std=c++11 -Wall -D_GLIBCXX_USE_CXX11_ABI=1
 LOCAL_CFLAGS += -O3
 LOCAL_CFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
 
