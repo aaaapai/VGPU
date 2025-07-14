9a6539921427e612470f3881cd1e11ccd91d987a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 01:28:05 +0800

    Update shaderconv.c

--- src/gl/shaderconv.c
+++ src/gl/shaderconv.c
@@ -12,6 +12,7 @@
 #include "loader.h"
 #include "pack/shaderconv.h"
 #include "../glsl/glsl_for_es.h"
+#include "../glx/hardext.h"
 
 typedef struct {
     const char* glname;
@@ -414,7 +415,7 @@ char * ConvertShaderConditionally(struct shader_s * shader_source){
     if(!shaderCompileStatus && globals4es.vgpu_backport) {
         shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
         shader_source->converted = ConvertShaderVgpu(shader_source);
-        shaderCompileStatus = testGenericShader(shader_source);
+        shaderCompileStatus = testGenericShader(shader_source, );
     }
 
     // At last resort, use forward porting
