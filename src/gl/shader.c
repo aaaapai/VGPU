ad047dd19dc3add3e82020917af35c12bad87cb1
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 14:37:56 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -235,21 +235,10 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
     	
     	
         // adapt shader if needed (i.e. not an es2 context and shader is not #version 100)
-        if(is_direct_shader(glshader->source))
-			glshader->converted = strdup(glshader->source);
-        else {
-            int glsl_version = getGLSLVersion(glshader->source);
-            DBG(SHUT_LOGD("[INFO] [Shader] Shader source: "))
-            DBG(SHUT_LOGD("%s", glshader->source))
-            if(glsl_version < 140 || globals4es.esversion < 300) {
-                glshader->converted = strdup(ConvertShaderConditionally(glshader));
-                glshader->is_converted_essl_320 = 0;
-            }
-            else {
-                char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
-                glshader->converted = strdup(result!=NULL?ConvertShaderConditionally(glshader):ConvertShaderConditionally(glshader));
-                glshader->is_converted_essl_320 = 1;
-	    }
+        if(glstate->glsl->es2 && !strncmp(glshader->source, "#version 100", 12))
+            glshader->converted = strdup(glshader->source);
+        else
+            glshader->converted = ConvertShader(glshader->source, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
 	}
             
