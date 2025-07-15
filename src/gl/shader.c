8949df26edae9995b9e51904ad71771a5ac7a548
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 22:10:18 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -242,11 +242,11 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
         DBG(SHUT_LOGD("[INFO] [Shader] Shader source: "))
         DBG(SHUT_LOGD("%s", glshader->source))
 	if ( glsl_version < 150 ) {
-	    glshader->converted = ConvertShader(glshader->source, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
+	    glshader->converted = ConvertShader(glshader->source, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 0);
 	} else {
             char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
             glshader->converted = strdup(result!=NULL?process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count):process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count));
-	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
+	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 1);
 	    glshader->is_converted_essl_320 = 1;
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
 	}
