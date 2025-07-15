b5a23e8630ebd6bbc81f9095c305fb6f7ecdbf22
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 09:28:41 +0800

    Update string_utils.c

--- src/gl/string_utils.c
+++ src/gl/string_utils.c
@@ -30,6 +30,42 @@ char* InplaceReplace(char* pBuffer, int* size, const char* S, const char* D)
     return pBuffer;
 }
 
+char * InplaceReplaceByIndex(char* pBuffer, int* size, const int startIndex, const int endIndex, const char* replacement)
+{
+    //SHUT_LOGD("BY INDEX: %s", replacement);
+    //SHUT_LOGD("BY INDEX: %i", strlen(replacement));
+
+    int length_difference;
+    if(endIndex < startIndex)
+        length_difference = strlen(replacement) + (endIndex - startIndex);
+    else if(endIndex == startIndex)
+        length_difference = strlen(replacement) - 1; // The initial char gets replaced
+    else
+        length_difference = strlen(replacement) - (endIndex - startIndex); // Can be negative if repl is smaller
+
+    pBuffer = ResizeIfNeeded(pBuffer, size, length_difference);
+    //SHUT_LOGD("BEFORE MOVING: \n%s", pBuffer);
+    // Move the end of the string
+    memmove(pBuffer + startIndex + strlen(replacement) , pBuffer + endIndex + 1, strlen(pBuffer) - endIndex + 1);
+    //SHUT_LOGD("AFTER MOVING 1: \n%s", pBuffer);
+
+    // Insert the replacement
+    memcpy(pBuffer + startIndex, replacement, strlen(replacement));
+    //strncpy(pBuffer + startIndex, replacement, strlen(replacement));
+    //SHUT_LOGD("AFTER MOVING 2: \n%s", pBuffer);
+
+    return pBuffer;
+}
+
+char * InplaceInsertByIndex(char * source, int *sourceLength, const int insertPoint, const char *insertedString){
+    int insertLength = strlen(insertedString);
+    source = ResizeIfNeeded(source, sourceLength, insertLength);
+    memmove(source + insertPoint + insertLength,  source + insertPoint, strlen(source) - insertPoint + 1);
+    memcpy(source + insertPoint, insertedString, insertLength);
+
+    return source;
+}
+
 char* InplaceInsert(char* pBuffer, const char* S, char* master, int* size)
 {
     char* m = ResizeIfNeeded(master, size, strlen(S));
