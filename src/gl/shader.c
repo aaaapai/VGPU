8cb0b40b5cc86c9dec629741447fd2705915536b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 14:42:53 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -287,7 +287,6 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
     GO(mvpmatrix)   \
     GO(notexarray)  \
     GO(clean)       \
-    GO(clipvertex) \
     GO2(texs)
 
 void accumShaderNeeds(GLuint shader, shaderconv_need_t *need) {
