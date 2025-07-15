aacfc834abd07db83b5cfd37f9a34ca5f9f53c89
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 14:44:01 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -238,7 +238,7 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
         if(glstate->glsl->es2 && !strncmp(glshader->source, "#version 100", 12))
             glshader->converted = strdup(glshader->source);
         else
-            glshader->converted = ConvertShader(glshader->source, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
+            glshader->converted = ConvertShader(glshader->source, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 0);
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
             
         // ======== add marker
