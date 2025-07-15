d159a2e8770461b23dff9b0ce662e3feba111297
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 18:53:40 +0800

    Update shaderconv.h

--- src/gl/shaderconv.h
+++ src/gl/shaderconv.h
@@ -52,6 +52,7 @@ int GetShaderVersion(const char * source);
 char * ReplaceFragmentOut(char * source, int *sourceLength);
 char * BackportConstArrays(char *source, int * sourceLength);
 char * ExtractString(char * source, int startString, int endString);
+char* ProcessSwitchCases(char* source, int* length);
 
 // const char* gl4es_convertARB(const char* source, GLboolean isVertex, char* erro_msg, int* error_pos); 
 #endif // _GL4ES_SHADERCONV_H_
