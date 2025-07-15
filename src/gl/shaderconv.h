9229f10d9110e3b34e9459d3290c2e4d65b130e2
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 09:21:34 +0800

    Update shaderconv.h

--- src/gl/shaderconv.h
+++ src/gl/shaderconv.h
@@ -18,5 +18,34 @@ const char* hasBuiltinAttrib(const char* vertexShader, int Att);
 const char* builtinAttribGLName(const char* name);
 const char* builtinAttribInternalName(const char* name);
 
+char * ForceIntegerArrayAccess(char* source, int * sourceLength);
+char * CoerceIntToFloat(char * source, int * sourceLength);
+char * ReplaceModOperator(char * source, int * sourceLength);
+char * WrapIvecFunctions(char * source, int * sourceLength);
+char * WrapFunction(char * source, int * sourceLength, char * functionName, char * wrapperFunctionName, char * wrapperFunction);
+int FindPositionAfterDirectives(char * source);
+int FindPositionAfterVersion(char * source);
+char * ReplaceGLFragData(char * source, int * sourceLength);
+char * ReplaceGLFragColor(char * source, int * sourceLength);
+char * ReplaceVariableName(char * source, int * sourceLength, char * initialName, char* newName);
+char * ReplaceFunctionName(char * source, int * sourceLength, char * initialName, char * finalName);
+char * RemoveUnsupportedExtensions(char * source);
+int doesShaderVersionContainsES(const char * source);
+char *ReplacePrecisionQualifiers(char *source, int *sourceLength, int isVertex);
+char * GetClosingTokens(char openingToken);
+int isOpeningToken(char openingToken);
+int GetClosingTokenPosition(const char * source, int initialTokenPosition);
+int GetClosingTokenPositionTokenOverride(const char * source, int initialTokenPosition, char initialToken);
+int GetNextTokenPosition(const char * source, int initialPosition, char token, const char * acceptedChars);
+void GetNextWord(char *source, int startPoint, int * startWord, int * endWord);
+unsigned long strstrPos(const char * haystack, const char * needle);
+char * insertIntAtFunctionCall(char * source, int * sourceSize, const char * functionName, int argumentPosition);
+char * InsertExtension(char * source, int * sourceLength, int insertPoint, const char * extension);
+char * InsertExtensions(char *source, int *sourceLength);
+int GetShaderVersion(const char * source);
+char * ReplaceFragmentOut(char * source, int *sourceLength);
+char * BackportConstArrays(char *source, int * sourceLength);
+char * ExtractString(char * source, int startString, int endString);
+
 // const char* gl4es_convertARB(const char* source, GLboolean isVertex, char* erro_msg, int* error_pos); 
 #endif // _GL4ES_SHADERCONV_H_
