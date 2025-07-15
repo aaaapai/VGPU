95e8ceaa455d40f5146726ee596f5bcefd4d07d2
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 20:36:15 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -243,7 +243,6 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
         DBG(SHUT_LOGD("%s", glshader->source))
             char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
             glshader->converted = strdup(result!=NULL?process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count):process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count));
-	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 1);
 	    glshader->is_converted_essl_320 = 1;
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
       }
@@ -263,7 +262,7 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
         Printf("\n========\n ");
 		*/
 		
-		shader_conv_(&glshader->source, &glshader->converted);
+	shader_conv_(&glshader->source, &glshader->converted);
 		
         //Printf("========NewConvertShader : \n %s \n========\n ", glshader->converted);
 		
