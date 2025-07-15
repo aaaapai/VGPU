1e26db2cc0f450791098a0c42274e3b92dac3884
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 20:16:58 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -241,16 +241,11 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
 	int glsl_version = getGLSLVersion(glshader->source);
         DBG(SHUT_LOGD("[INFO] [Shader] Shader source: "))
         DBG(SHUT_LOGD("%s", glshader->source))
-        if(glsl_version < 140 || globals4es.esversion < 300) {
-                glshader->converted = strdup(ConvertShaderConditionally(glshader));
-                glshader->is_converted_essl_320 = 0;
-        } else {
             char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
             glshader->converted = strdup(result!=NULL?process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count):ConvertShaderConditionally(glshader));
 	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 1);
 	    glshader->is_converted_essl_320 = 1;
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
-        }
       }
         // ======== add marker
         add_marker(&glshader->converted);
