23b286857420290a23562635b8611bf1eac9111a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 18:30:18 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -247,7 +247,7 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
         } else {
             char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
             glshader->converted = strdup(result!=NULL?process_uniform_declarations(result, glshader->uniforms_declarations, &glshader->uniforms_declarations_count):ConvertShaderConditionally(glshader));
-	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
+	    glshader->converted = ConvertShader(glshader->converted, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need, 1);
 	    glshader->is_converted_essl_320 = 1;
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
         }
