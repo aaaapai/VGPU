7de9d1dcc0b0342b754d454531e95c087010aaf3
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-15 15:33:42 +0800

    Update shader.c

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -236,14 +236,15 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
     	
         // adapt shader if needed (i.e. not an es2 context and shader is not #version 100)
         if(glstate->glsl->es2 && !strncmp(glshader->source, "#version 100", 12))
+	{
             glshader->converted = strdup(glshader->source);
 	    glshader->is_converted_essl_320 = 0;
-        else
-	    char* result = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
+        } else {
+	    glshader->converted = GLSLtoGLSLES(glshader->source, glshader->type, globals4es.esversion);
             glshader->converted = ConvertShader(result, glshader->type==GL_VERTEX_SHADER?1:0, &glshader->need);
 	    glshader->is_converted_essl_320 = 1;
             DBG(SHUT_LOGD("\n[INFO] [Shader] Converted Shader source: \n%s", glshader->converted))
-            
+        }
         // ======== add marker
         add_marker(&glshader->converted);
         // ======== Handling the first half of an implicit type conversion.
